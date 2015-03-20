8ae4335482604bf8267f0dbaa720920a7ea7dcbb
KReguieg <Khaled.Reguieg@gmail.com>
2015-03-20 19:26:28 +0100

    [ONBACKPRESSED] ADD a onBackPressed method which handled the program exit and more facts

--- app/src/main/java/de/flowment/funfacts/FunFactsActivity.java
+++ app/src/main/java/de/flowment/funfacts/FunFactsActivity.java
@@ -2,17 +2,21 @@ package de.flowment.funfacts;
 
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class FunFactsActivity extends ActionBarActivity {
 
     private FactBook mFactBook = new FactBook();
     private ColorWheel mColor = new ColorWheel();
+    private boolean doubleBackToExitPressedOnce;
+    private Handler mHandler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +27,6 @@ public class FunFactsActivity extends ActionBarActivity {
         final TextView factLbl = (TextView) findViewById(R.id.funFactTxt);
         final Button showFactBtn = (Button) findViewById(R.id.nextFunFactBtn);
         final Resources res = getResources();
-        final int[] colors = res.getIntArray(R.array.androidcolors);
         final RelativeLayout rl = (RelativeLayout) findViewById(R.id.relativeLayoutId);
 
         View.OnClickListener listener = new View.OnClickListener() {
@@ -39,5 +42,33 @@ public class FunFactsActivity extends ActionBarActivity {
         };
 
         showFactBtn.setOnClickListener(listener);
+    }
+
+    private final Runnable mRunnable = new Runnable() {
+        @Override
+        public void run() {
+            doubleBackToExitPressedOnce = false;
+        }
+    };
+
+    @Override
+    protected void onDestroy()
+    {
+        super.onDestroy();
+
+        if (mHandler != null) { mHandler.removeCallbacks(mRunnable); }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        mHandler.postDelayed(mRunnable, 2000);
     }
 }
