64e83830e4d2b9286b7078e6d23bedb64f10de56
KReguieg <Khaled.Reguieg@gmail.com>
2015-04-08 18:20:04 +0200

    [NEWACTIVTY] Added a new Activty which allows users to submit their own funFact

---
+++ app/src/main/java/de/flowment/funfacts/addNewFunFactActivity.java
@@ -0,0 +1,15 @@
+package de.flowment.funfacts;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+
+
+public class AddNewFunFactActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_new_fun_fact);
+
+    }
+}
